31f40a8f7f3f8a6631c1748513d200a1d7f3a7c3
MortalWombat-repo <69204832+MortalWombat-repo@users.noreply.github.com>
2025-07-16 21:59:33 +0200

    Update postInstall.sh

--- .streamlit/postInstall.sh
+++ .streamlit/postInstall.sh
@@ -1,4 +1,6 @@
+# .streamlit/postInstall.sh
 #!/bin/bash
-echo "Downloading SpaCy Croatian model..."
-python3 -m spacy download hr_core_news_md
-echo "Done."
+# Activate the virtual environment
+source /home/adminuser/venv/bin/activate
+# Install hr_core_news_md
+python -m pip install https://github.com/explosion/spacy-models/releases/download/hr_core_news_md-3.8.0/hr_core_news_md-3.8.0-py3-none-any.whl
