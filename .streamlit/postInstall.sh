54ed9106b5d2b9c51ee24a54de31e6d7ef33e748
MortalWombat-repo <sarruicon@gmail.com>
2025-07-16 22:10:55 +0200

    fixing sh

--- .streamlit/postInstall.sh
+++ .streamlit/postInstall.sh
@@ -1,6 +1,6 @@
 # .streamlit/postInstall.sh
 #!/bin/bash
 # Activate the virtual environment
-source /home/adminuser/venv/bin/activat
+source /home/adminuser/venv/bin/activate
 # Install hr_core_news_md
 python -m pip install https://github.com/explosion/spacy-models/releases/download/hr_core_news_md-3.8.0/hr_core_news_md-3.8.0-py3-none-any.whl
